0eac0c4557dad95d7c6b8035ae43cf496fa904d5
Steve Carleton <steve.carleton@outlook.com>
2024-11-25 13:01:49 -0500

    Refactor the validation and logic into a seperate class

---
+++ MessengerHost.cs
@@ -0,0 +1,62 @@
+﻿namespace Scc.Mastermind;
+
+class MastermindHost
+{
+    const int maxAttempts = 10;
+    const string secret = "1234";
+    const string banner = "An input must be valid to count as an attempt";
+    const string prompt = ": Enter four digits, with each digit ranging from 1 to 6.";
+    const string successMessage = "SUCCESS!";
+    const string failureMessage = "FAILURE";
+    const char plusMarker = '+';
+    const char minusMarker = '-';
+    const int minDigitValue = 1;
+    const int maxDigitValue = 6;
+
+    static bool validInput = false;
+    static int attempts = 0;
+    static bool match = false;
+    static string? input = null;
+
+    static void Main()
+    {
+        Console.WriteLine($"{banner}");
+
+        while (attempts < maxAttempts && !match)
+        {
+            while (!validInput)
+            {
+                Console.WriteLine($"{attempts + 1}{prompt}");
+                input = Console.ReadLine();
+
+                validInput = MastermindLogic.ValidateInput(input, secret, minDigitValue, maxDigitValue);
+                if (validInput)
+                {
+                    attempts++;
+                }
+            }
+
+            string messageStr = MastermindLogic.ProcessInput(input, secret, successMessage, plusMarker, minusMarker);
+
+            if (messageStr != null)
+            {
+                Console.WriteLine(messageStr);
+            }
+
+            if (messageStr == successMessage)
+            {
+                match = true;
+            }
+            else
+            {
+                validInput = false;
+            }
+
+            if (attempts == maxAttempts)
+            {
+                Console.WriteLine(failureMessage);
+                return;
+            }
+        }
+    }
+}
--- MessengerLogic.cs
+++ MessengerLogic.cs
@@ -1,68 +1,13 @@
-﻿class Messenger
+﻿namespace Scc.Mastermind;
+
+public class MastermindLogic
 {
-    const int maxAttempts = 10;
-    const string secret = "1234";
-    const string banner = "An input must be valid to count as an attempt";
-    const string prompt = ": Enter four digits, with each digit ranging from 1 to 6.";
-    const string successMessage = "SUCCESS!";
-    const string failureMessage = "FAILURE";
-    const char plusMarker = '+';
-    const char minusMarker = '-';
-    const int minDigitValue = 1;
-    const int maxDigitValue = 6;
-
-    static bool validInput = false;
-    static int attempts = 0;
-    static bool match = false;
-    static string? input = null;
-
-    static void Main()
-    {
-        Console.WriteLine($"{banner}");
-
-        while (attempts < maxAttempts && !match)
-        {
-            while (!validInput)
-            {
-                Console.WriteLine($"{attempts + 1}{prompt}");
-                input = Console.ReadLine();
-
-                validInput = ValidateInput(input);
-                if (validInput)
-                {
-                    attempts++;
-                }
-            }
-
-            string messageStr = ProcessInput(input);
-            if (messageStr != null)
-            {
-                Console.WriteLine(messageStr);
-            }
-
-            if (messageStr == successMessage)
-            {
-                match = true;
-            }
-            else
-            {
-                validInput = false;
-            }
-
-            if (attempts == maxAttempts)
-            {
-                Console.WriteLine(failureMessage);
-                return;
-            }
-        }
-    }
-
     /// <summary>
     /// Check that the required number of digits have been entered, in the given range of values.
     /// </summary>
     /// <param name="input">Input string for validation</param>
     /// <returns>Validity flag</returns>
-    public static bool ValidateInput(string? input)
+    public static bool ValidateInput(string? input, string secret, int minDigitValue, int maxDigitValue)
     {
         if (input?.Length == secret.Length)
         {
@@ -88,13 +33,18 @@
     }
 
     /// <summary>
-    /// Check if the input matches the secret. If not determine apply logic such that a plus sign should be output 
+    /// Check if the input matches the secret. If not apply logic such that a plus sign should be output 
     /// for every digit that is both correct and in the correct position.  Output all plus signs first, all minus 
     /// signs second, and nothing for incorrect digits.
     /// </summary>
     /// <param name="input">Input string for processing</param>
-    /// <returns>Processing result</returns>
-    public static string ProcessInput(string? input)
+    /// <returns>Processing result string</returns>
+    public static string ProcessInput(
+        string? input,
+        string secret,
+        string successMessage,
+        char plusMarker,
+        char minusMarker)
     {
         int plusses = 0;
         int minusses = 0;
